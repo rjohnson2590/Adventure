4b7b504a6b4eb6a0660a3d5a4dde88bd0d4420e2
Richard Johnson <rjohnson2590@gmail.com>
2015-05-12 17:07:04 -0700

    Works with map, add inventory by trying to mess with the json file

--- newAdventure.js
+++ newAdventure.js
@@ -1,17 +1,39 @@
 console.log(require('./node_modules/jsi-gamelib').map(require(process.argv[2])));
 
+var contents = require("./game.json") 
+var components = require("./node_modules/jsi-gamelib/lib/components.js");
 var readline = require('readline');
+var draw= require('jsi-gamelib').map;
+var newMap= require('jsi-gamelib').map.drawMap;
+var drawing=require("./node_modules/jsi-gamelib/lib/map.js")
 
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-var contents = require("./game.json") 
-
 var start=contents["rooms"][0].name;
 
 var roomIndex= {A:0, B:1, C:2, D:3, E:4, F:5, G:6, H:7, I:8, J:9, K:10}
+
+var doodle= draw(contents);
+
+// var visited=[0,3]
+
+// var Tom= visited.forEach(function(number){contents.rooms[number].map = components.createRoom(contents.rooms[number])})
+
+var house={rooms:[contents["rooms"][0]]}
+// house.rooms.push()
+// function work(array){
+// 	array.forEach(function(element,index){
+// 	console.log(contents["rooms"][element].drawing)
+// })
+// }
+
+// function camel(){for(var i=0; i<visited.length;i++){
+// 	console.log((contents["rooms"][visited[i]].map))
+// }}
+
 
 
 function name1(letter){
@@ -22,6 +44,7 @@ function room(index){
 	return contents["rooms"][index].name;
 }
 
+
 function movement(index, direction){
 	return contents["rooms"][index][direction];
 }
@@ -30,14 +53,19 @@ function description(index){
 	return contents["rooms"][index].description;
 }
 
-function booty(){
-	return contents["rooms"][10].treasure;
+function booty(index){
+	return contents["rooms"][index].treasure;
 }
 
+function gameMap(A){
+	console.log(contents.rooms[name1(A)].drawing)
+	// console.log(contents.rooms[name1(B)].drawing);
+}
 
-
+// console.log(actualVisted.drawing)
 
 function game(A){
+	console.log(drawing(house));
 	rl.question(description(name1(A)),function(choice){
 		if(choice==="east"){
 			var dir='';
@@ -45,7 +73,14 @@ function game(A){
 			dir = movement(name1(A),choice);
 			if(dir!==null){	
 			other=name1(dir)
-			game(room(other));}
+			house.rooms.push(contents["rooms"][other])
+			// console.log(actualVisted.drawing)
+			game(room(other))
+			if(booty(other)=== true){
+				console.log("You found the treasure and won the game!!!")
+				rl.close();
+			}
+			;}
 			else{console.log("That is not a valid way to go, I am teleporting you back to the start");
 				game("A")			}
 		}else if(choice==="north"){
@@ -54,6 +89,8 @@ function game(A){
 			dir = movement(name1(A),choice); 
 			if(dir!==null){	
 			other=name1(dir)
+			house.rooms.push(contents["rooms"][other])
+			// console.log(actualVisted.drawing)
 			game(room(other));}
 			else{console.log("That is not a valid way to go, I am teleporting you back to the start");
 				game("A")			}
@@ -63,6 +100,8 @@ function game(A){
 			dir = movement(name1(A),choice);
 			if(dir!==null){	
 			other=name1(dir)
+			house.rooms.push(contents["rooms"][other])
+			// console.log(actualVisted.drawing)
 			game(room(other));}
 			else{console.log("That is not a valid way to go, I am teleporting you back to the start");
 				game("A")			}
@@ -72,6 +111,9 @@ function game(A){
 			dir = movement(name1(A),choice);
 			if(dir!==null){	
 			other=name1(dir)
+			house.rooms.push(contents["rooms"][other])
+			// console.log(actualVisted.drawing)
+			// console.log(contents.rooms[other].drawing)
 			game(room(other));}
 			else{console.log("That is not a valid way to go, I am teleporting you back to the start");
 				game("A")			}
@@ -81,9 +123,11 @@ function game(A){
 	})
 }
 
+// console.log(gameMap("A","D"));
+
 game("A");
 
-
+// ~/Adventure/node_modules/jsi-gamelib/lib
 
 // console.log(movement(0, "north"))
 
