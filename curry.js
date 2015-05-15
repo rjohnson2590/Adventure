1da817cbc81ee5d8092ee2df47c99d5b9acb4ad7
Richard Johnson <rjohnson2590@gmail.com>
2015-05-14 17:26:06 -0700

    flatten shit too

--- curry.js
+++ curry.js
@@ -1,14 +1,55 @@
-var array= [1,2,3,4,5]
-var newArray=[]
+// var array= [1,2,3,4,5]
+// var newArray=[]
 
-function squareAll(element,ray){
-	return ray.push(element*element)
+// function squareAll(element,ray){
+// 	return ray.push(element*element)
+// }
+
+
+
+// array.forEach(function(x){
+// 	squareAll(x,newArray)
+// })
+
+// console.log(newArray)
+
+var _=require("lodash")
+
+
+var array=[1,[2,3,[4]]];
+// var newArray=[];
+// var value;
+// var value1;
+
+// function flat(ray){
+// 	value=ray.pop();
+// 	value1=ray.shift();
+// 	value.unshift(value1)
+// 	return value
+// }
+
+var flat= function(array,isDeep,start){
+	var output=[], idx=0;
+	for(var i= 0; i < array.length; i++){
+		value = array[i];
+		if(!isDeep){ value=flat(value,isDeep)
+		var j=0, len= value.length;
+		output.length +=len;
+		while(j<len){
+			console.log("hello")
+			output[idx++]=value[j++]
+		}
+	}	else{
+		output[idx++]=value;
+
+	}
+}
+	return output;
 }
 
 
+//instance off
 
-array.forEach(function(x){
-	squareAll(x,newArray)
-})
+console.log(flat(array,false))
 
-console.log(newArray)
+console.log(_.flatten(array,true))
--- fib.js
+++ fib.js
@@ -53,24 +53,36 @@
 
 
 
-var numbers = [1, 2, 3, 4, 5, 6];
-var newNumbers=[]
-var filter = function(array, callBack, ray){
-	array.forEach(function(n){
-		if(callBack(n)===true){
-		ray.push(n)
-			}
-	})
-	return ray
-}
-var evenNumbers = filter(numbers, function(n) { return (n % 2) === 0; },newNumbers);
+// var numbers = [1, 2, 3, 4, 5, 6];
+// var newNumbers=[]
+// var filter = function(array, callBack, ray){
+// 	array.forEach(function(n){
+// 		if(callBack(n)===true){
+// 		ray.push(n)
+// 			}
+// 	})
+// 	return ray
+// }
+// var evenNumbers = filter(numbers, function(n) { return (n % 2) === 0; },newNumbers);
 
 
-console.log(evenNumbers)
+// console.log(evenNumbers)
 
+// var pairs = [['name', 'JSI'], ['location', { city: 'Portland', 'state': 'OR' }], ['school', 'PCS']];
 
+// function reduce(array, callBack, start) {
+//   var current = start;
+//   for (var i = 0; i < array.length; i++)
+//     current = callBack(current, array[i]);
+//   return current;
+// }
 
-
+// console.log(reduce(pairs,function(accum,curr){
+// 	var key= curr[0];
+// 	var value= curr[1];
+// 	accum[key]=value	 
+// 	return accum
+// },{}))
 
 
 
